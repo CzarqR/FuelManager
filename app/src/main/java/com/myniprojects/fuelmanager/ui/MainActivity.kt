d933e4ea016e1bed5e11d857fdf6b22a6c74a1c3
CzarqR <czarekr98@gmail.com>
2020-07-07 01:11:38 +0200

    Navigation bar

--- app/src/main/java/com/myniprojects/fuelmanager/ui/MainActivity.kt
+++ app/src/main/java/com/myniprojects/fuelmanager/ui/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.myniprojects.fuelmanager.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.myniprojects.fuelmanager.R
+import com.myniprojects.fuelmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // setup navigation bar
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
+
+
+    override fun onSupportNavigateUp(): Boolean
+    {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
+    }
+
 }
