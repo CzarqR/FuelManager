c13fb960d3971e6557a7ac1ce71d415a11e4c81f
CzarqR <czarekr98@gmail.com>
2020-08-03 20:31:04 +0200

    Icon set

--- app/src/main/java/com/myniprojects/fuelmanager/model/CarIcon.kt
+++ app/src/main/java/com/myniprojects/fuelmanager/model/CarIcon.kt
@@ -2,18 +2,28 @@ package com.myniprojects.fuelmanager.model
 
 import com.myniprojects.fuelmanager.R
 
-data class CarIcon(val icon: Int, val name: Int)
+data class CarIcon(val icon: Int)
 {
     companion object
     {
         val cars: Array<CarIcon> = arrayOf(
-            CarIcon(R.drawable.car0, R.string.car0),
-            CarIcon(R.drawable.car1, R.string.car1),
-            CarIcon(R.drawable.car2, R.string.car2),
-            CarIcon(R.drawable.car3, R.string.car3),
-            CarIcon(R.drawable.car4, R.string.car4),
-            CarIcon(R.drawable.car5, R.string.car5),
-            CarIcon(R.drawable.car6, R.string.car6)
+            CarIcon(R.drawable.car0),
+            CarIcon(R.drawable.car1),
+            CarIcon(R.drawable.car2),
+            CarIcon(R.drawable.car3),
+            CarIcon(R.drawable.car4),
+            CarIcon(R.drawable.car5),
+            CarIcon(R.drawable.car6),
+            CarIcon(R.drawable.bus),
+            CarIcon(R.drawable.bus1),
+            CarIcon(R.drawable.motorcycle),
+            CarIcon(R.drawable.scooter),
+            CarIcon(R.drawable.scooter1),
+            CarIcon(R.drawable.taxi),
+            CarIcon(R.drawable.truck),
+            CarIcon(R.drawable.truck1),
+            CarIcon(R.drawable.truck2),
+            CarIcon(R.drawable.van)
         )
     }
 }
